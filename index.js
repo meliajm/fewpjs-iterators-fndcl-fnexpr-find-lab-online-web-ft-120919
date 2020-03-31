1391459ff735a06ad07b975822df2ca153b04e3e
github username <meliajm@gmail.com>
2020-03-31 15:16:58 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function 
+// () {
+//   return "hi"
+// }
+
+let superbowlWin = (record) => {
+  const record_row = (record.find(function(s) { return s.result === 'W'}))
+  if (record_row === undefined) {
+    return undefined
+  } else {
+    return record_row.year
+  }
+
 }
